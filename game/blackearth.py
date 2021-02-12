b3c61052b147646dc9beea91d7754e5094acefad
cgrooves <caleb.j.groves@gmail.com>
2021-02-12 15:12:43 -0700

    Add another tank
    Pretty ugly coding, but just adds in the second tank
    in order to close out Player tanks on main window #4

--- game/blackearth.py
+++ game/blackearth.py
@@ -26,27 +26,33 @@ class BlackEarthGame(arcade.Window):
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
 
-        # Set up player
-        self.player = Tank(
-            Vec2d(SCREEN_WIDTH/2,SCREEN_HEIGHT/3),
+        # Set up player1
+        self.player1 = Tank(
+            Vec2d(SCREEN_WIDTH/3,SCREEN_HEIGHT/3),
             color=arcade.csscolor.DARK_RED
             )
+        
+        self.player2 = Tank(
+            Vec2d(SCREEN_WIDTH*2/3, SCREEN_HEIGHT/3),
+            color=arcade.csscolor.DARK_BLUE
+        )
+        self.player2.turretAngleDeg = 90 + 45
 
     def on_key_press(self, key, modifiers):
-        # Pass through inputs to player
-        self.player.on_key_press(key, modifiers)
+        # Pass through inputs to player1
+        self.player1.on_key_press(key, modifiers)
     
     def on_key_release(self, key, modifiers):
         # Handle window events first
         if key == arcade.key.ESCAPE:
             exit()
 
-        # Pass through inputs to player
-        self.player.on_key_release(key, modifiers)
+        # Pass through inputs to player1
+        self.player1.on_key_release(key, modifiers)
 
     def on_update(self, delta_time):
         # Update game state for game objects
-        self.player.on_update()
+        self.player1.on_update()
         self.shapes = arcade.ShapeElementList()
         
         # Create the ground
@@ -65,12 +71,13 @@ class BlackEarthGame(arcade.Window):
         arcade.start_render()
         # Code to draw the screen goes here
 
-        # Render the player
-        self.player.draw()
+        # Render the player1
+        self.player1.draw()
+        self.player2.draw()
 
-        # Render the player's tank angle
+        # Render the player1's tank angle
         arcade.draw_text(
-            text=f"Tank Angle: {self.player.turretAngleDeg}",
+            text=f"Tank Angle: {self.player1.turretAngleDeg}",
             start_x =10.0,
             start_y=0.95*SCREEN_HEIGHT,
             color=arcade.csscolor.WHITE_SMOKE
