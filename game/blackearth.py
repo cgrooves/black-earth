03a4bd9d880326a486cf56ef7664bd9594f38cbd
cgrooves <caleb.j.groves@gmail.com>
2021-03-02 18:38:15 -0700

    Cgrooves/issue5 (#12)
    * First pass make tank with turret
    
    Controller sucks, but hey, it draws a "tank", and
    you can change the turret angle.
    
    * A slightly improved keyboard interface
    
    * Separate tank out into module
    
    * Add some explanatory comments
    
    * Escape key exits program
    
    * A potential fix, maybe not
    
    Not sure if this math is right
    
    * Controllable tank angle
    Fixes #5
    
    * Implement simple text display for angle
    
    There's probably a better way to implement this using the
    `arcade.gui.UILabel` module, but this is fine for now.
    
    * Change sky color to something a little darker
    
    * Add another tank
    
    Pretty ugly coding, but just adds in the second tank
    in order to close out Player tanks on main window #4

--- game/blackearth.py
+++ game/blackearth.py
@@ -2,11 +2,14 @@
 Hello-World example given from https://arcade.academy/examples/platform_tutorial/step_01.html
 """
 import arcade
+from pymunk import Vec2d
+
+from tank import Tank
 
 # Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer"
+SCREEN_TITLE = "Black Earth"
 
 class BlackEarthGame(arcade.Window):
     """
@@ -18,10 +21,38 @@ class BlackEarthGame(arcade.Window):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
+
+        # Set up player1
+        self.player1 = Tank(
+            Vec2d(SCREEN_WIDTH/3,SCREEN_HEIGHT/3),
+            color=arcade.csscolor.DARK_RED
+            )
+        
+        self.player2 = Tank(
+            Vec2d(SCREEN_WIDTH*2/3, SCREEN_HEIGHT/3),
+            color=arcade.csscolor.DARK_BLUE
+        )
+        self.player2.turretAngleDeg = 90 + 45
+
+    def on_key_press(self, key, modifiers):
+        # Pass through inputs to player1
+        self.player1.on_key_press(key, modifiers)
+    
+    def on_key_release(self, key, modifiers):
+        # Handle window events first
+        if key == arcade.key.ESCAPE:
+            exit()
+
+        # Pass through inputs to player1
+        self.player1.on_key_release(key, modifiers)
+
+    def on_update(self, delta_time):
+        # Update game state for game objects
+        self.player1.on_update()
         self.shapes = arcade.ShapeElementList()
         
         # Create the ground
@@ -40,6 +71,17 @@ class BlackEarthGame(arcade.Window):
         arcade.start_render()
         # Code to draw the screen goes here
 
+        # Render the player1
+        self.player1.draw()
+        self.player2.draw()
+
+        # Render the player1's tank angle
+        arcade.draw_text(
+            text=f"Tank Angle: {self.player1.turretAngleDeg}",
+            start_x =10.0,
+            start_y=0.95*SCREEN_HEIGHT,
+            color=arcade.csscolor.WHITE_SMOKE
+        )
         self.shapes.draw()
 
 
---
+++ game/tank.py
@@ -0,0 +1,93 @@
+import arcade
+from pymunk import Vec2d
+
+TANK_SIZE = 50
+TANK_TURRET_LENGTH = 40
+TANK_STARTING_ANGLE_DEG = 45
+TURRET_ANGLE_MAX = 180
+TURRET_ANGLE_MIN = 0
+TURRET_WIDTH = 5
+TURRET_OFFSET_Y = 5
+TURRET_SPEED = 2
+
+class Tank:
+    """
+    Class encapsulating a player Tank
+    """
+
+    def __init__(self, position: Vec2d, color: arcade.color):
+        """
+        Construct the tank with a position and color
+        """
+        self.size = TANK_SIZE
+        self.position = position
+        self.color = color
+        self.turretAngleDeg = TANK_STARTING_ANGLE_DEG
+        self.turretLength = TANK_TURRET_LENGTH
+        self.moveTurret = 0
+
+    def draw(self):
+        """
+        Render the tank body and turret
+        """
+        # Draw tank body
+        arcade.draw_arc_filled(
+            center_x=self.position.x,
+            center_y=self.position.y,
+            width=self.size,
+            height=self.size,
+            color=self.color,
+            start_angle=0.0,
+            end_angle=180
+        )
+        # Draw turret
+        # Calculate turret end point
+        turretPosition = Vec2d(self.turretLength, 0)
+        turretPosition.rotate_degrees(self.turretAngleDeg)
+        arcade.draw_line(
+            start_x=self.position.x,
+            start_y=self.position.y + TURRET_WIDTH/2,
+            end_x=turretPosition.x + self.position.x,
+            end_y=turretPosition.y + self.position.y + TURRET_WIDTH/2,
+            color=self.color,
+            line_width=TURRET_WIDTH
+        )
+    
+    def on_key_press(self, key, modifiers):
+        """
+        Handle key presses.
+        
+        If a key is pressed, we'll set a turret movement
+        speed. We can't just move the turret, because otherwise the turret will
+        only move each and every time that we press a key (meaning, we have to
+        press, release, press, release, just to move twice). Instead, we'll change
+        the turret's movement speed based on which keys are pressed.
+        """
+        if key == arcade.key.LEFT:
+            self.moveTurret += TURRET_SPEED
+        if key == arcade.key.RIGHT:
+            self.moveTurret -= TURRET_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """
+        Handle key releases.
+
+        Decrement the turret speed
+        """
+        if key == arcade.key.LEFT:
+            self.moveTurret -= TURRET_SPEED
+        if key == arcade.key.RIGHT:
+            self.moveTurret += TURRET_SPEED
+
+    def on_update(self):
+        """
+        Update the player state
+
+        Update the turret angle (and bound it).
+        """
+        if self.moveTurret != 0:
+            self.turretAngleDeg += self.moveTurret
+        if self.turretAngleDeg > TURRET_ANGLE_MAX:
+            self.turretAngleDeg = TURRET_ANGLE_MAX
+        elif self.turretAngleDeg < TURRET_ANGLE_MIN:
+            self.turretAngleDeg = TURRET_ANGLE_MIN
