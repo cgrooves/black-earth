d71e8dcd0dc1d8cb40efcbb58180f95a553039e5
cgrooves <caleb.j.groves@gmail.com>
2021-03-25 22:18:56 -0700

    Cgrooves/issue9 (#14)
    * First pass make tank with turret
    
    Controller sucks, but hey, it draws a "tank", and
    you can change the turret angle.
    
    * A slightly improved keyboard interface
    
    * Separate tank out into module
    
    * Add some explanatory comments
    
    * Escape key exits program
    
    * A potential fix, maybe not
    
    Not sure if this math is right
    
    * Controllable tank angle
    Fixes #5
    
    * Implement simple text display for angle
    
    There's probably a better way to implement this using the
    `arcade.gui.UILabel` module, but this is fine for now.
    
    * Change sky color to something a little darker
    
    * Add another tank
    
    Pretty ugly coding, but just adds in the second tank
    in order to close out Player tanks on main window #4
    
    * Make a Player list, put shapes list in setup
    
    The shapes list needed to be in setup, and make the players into
    a list instead of hard-coded each one.
    
    * Add switching logic on spacebar
    
    * Make num of tanks configurable
    
    * Switch to using a cycle for cycling tanks
    
    The itertools.cycle is neater than the logic I implemented,
    so switch over to that.
    
    * Give a fix to the turret bug
    
    I think that the turret bug was because I incremented or decremented the
    turret speed based on key press/release. If the key release happens to
    not have registered when the spacebar is pressed, or just at any time,
    then there's no way to stop the turret from moving. This proposes a fix
    for Tank turret stops accepting input, stuck high or low #15, by setting
    the speed on key press and zeroing it on key release.
    
    * Add more explanatory "why" comments
    
    * Simplify import statements
    
    Since a huge part of this project is for learning, I think that it's a
    good idea to just keep things simple by making a rule for ourselves
    that, unless something is a super long use statements (i.e. if we're
    having to type somepackage.foobar.baz.bin.AwesomeFunction, which would
    warrant a statements like "from somepackage.foobar.baz.bin import
    AwesomeFunction" so that we don't have to type out the long thing every
    single time we want to use AwesomeFunction), just do a simple "import
    <package>" statement. That way, its clear where things are coming from.
    
    * Add some more clarifying comments to Tank

--- game/blackearth.py
+++ game/blackearth.py
@@ -1,10 +1,16 @@
 """
 Hello-World example given from https://arcade.academy/examples/platform_tutorial/step_01.html
 """
-import arcade
-from pymunk import Vec2d
 
-from tank import Tank
+# General import statements
+import itertools
+
+# Third-party library import statements
+import arcade
+import pymunk
+
+# Local import statements
+import tank
 
 # Constants
 SCREEN_WIDTH = 1000
@@ -17,45 +23,59 @@ class BlackEarthGame(arcade.Window):
     """
 
     def __init__(self):
+        """Constructor"""
 
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
-    def setup(self):
-        """ Set up the game here. Call this function to restart the game. """
+    def setup(self, num_tanks=2):
+        """ Set up the game here. Call this function to restart the game.
+        
+        Allow the user to specify how many tanks to start the game
+        with. Note that the way that we do this will DEFINITELY change
+        in the future, this is just a fun way to be able to parameterize
 
-        # Set up player1
-        self.player1 = Tank(
-            Vec2d(SCREEN_WIDTH/3,SCREEN_HEIGHT/3),
-            color=arcade.csscolor.DARK_RED
+
+        @param num_tanks Number of tanks to instantiate
+        """
+
+        # Set up players
+
+        # Populate a list based on number of tanks
+        # The list will be useful for keeping track of all of the tanks, and
+        # being able to do the same operation (such a "draw") on them all very
+        # easily.
+        self.tanksList = []
+        for n in range(1,num_tanks + 1):
+            new_tank = tank.Tank(
+                name=f"Player {n}",
+                position = pymunk.Vec2d(SCREEN_WIDTH*n/(num_tanks+1), SCREEN_HEIGHT/3),
+                color=next(tank.TANK_COLORS)
             )
-        
-        self.player2 = Tank(
-            Vec2d(SCREEN_WIDTH*2/3, SCREEN_HEIGHT/3),
-            color=arcade.csscolor.DARK_BLUE
-        )
-        self.player2.turretAngleDeg = 90 + 45
 
-    def on_key_press(self, key, modifiers):
-        # Pass through inputs to player1
-        self.player1.on_key_press(key, modifiers)
-    
-    def on_key_release(self, key, modifiers):
-        # Handle window events first
-        if key == arcade.key.ESCAPE:
-            exit()
+            self.tanksList.append(new_tank)
 
-        # Pass through inputs to player1
-        self.player1.on_key_release(key, modifiers)
+        # Create a circular Iterator for the tank list
+        # An Iterator is something different from a list, though certainly you can
+        # "iterate" through things like lists and tuples and even dictionaries.
+        # In other words, lists and tuples and dictionaries can themselves be termed
+        # "Iterators". I still want a list of tanks, but I also want something that
+        # points to that list, but I can use to cycle through them endlessly. I know
+        # that I want this, because I know that I just want to be able to keep going
+        # to whatever Tank's turn is next. Next, next, next. Me calling "next" shouldn't
+        # alter the list of tanks; I can have two "views" of the same data: the list
+        # view, and the endless cycle view.
+        self.tanksCycle = itertools.cycle(self.tanksList)
 
-    def on_update(self, delta_time):
-        # Update game state for game objects
-        self.player1.on_update()
+        # Set the active player
+        self.activeTank = next(self.tanksCycle)
+
+        # Set up other shapes to draw (just the ground for now)
         self.shapes = arcade.ShapeElementList()
-        
-        # Create the ground
+
+        # Create the ground (just a rectangle for now)
         ground = arcade.create_rectangle_filled(
             center_x=SCREEN_WIDTH / 2,
             center_y=SCREEN_HEIGHT / 6,
@@ -64,6 +84,51 @@ class BlackEarthGame(arcade.Window):
             color=arcade.color.DARK_SPRING_GREEN
         )
         self.shapes.append(ground)
+    
+    def on_key_press(self, key, modifiers):
+        """Handle key press events"""
+
+        # Pass through inputs to activeTank
+
+        # Instead of having the Window object handle all of the logic for
+        # updating the Tank's turret, we can use the fact that the Tank is
+        # a class, and can define its own behaviors (e.g. functions, or methods),
+        # that we can then use. So the idea here is to pass the key event on to
+        # the Tank, and let it update itself. That way, if somebody really
+        # wants to know exactly what the Tank is doing with key presses, they can
+        # go look. Otherwise, if they just want to know the general flow of the
+        # game, they can look here and say "oh look, the Tank handles keyboard events".
+        # This is an example of the idea of "encapsulation", an important principle
+        # in programming. Encapsulation states that we should try to group similar
+        # data and behaviors and put them into a box, with only useful interfaces
+        # exposed. Think of a car: we encapsulate the workings of the engine and
+        # the starter and just give people a key to turn. Then, if they want to know
+        # more, or things aren't working, they can pop the hood and look in.
+        self.activeTank.on_key_press(key, modifiers)
+    
+    def on_key_release(self, key, modifiers):
+        """Handle key release events"""
+
+        # Handle window events first
+        if key == arcade.key.ESCAPE:
+            exit()
+
+        # Pass through inputs to activeTank
+        self.activeTank.on_key_release(key, modifiers)
+
+        # Handle space bar
+        # I'm putting this code here, after passing the key press and key release
+        # onto the active tank, so that the tank can do its own events with the
+        # spacebar before we "pass the control"
+        if key == arcade.key.SPACE:
+            # Actually set the active tank
+            self.activeTank = next(self.tanksCycle)
+
+    def on_update(self, delta_time):
+        """ Update game state for game objects """
+
+        # Make the active tank update
+        self.activeTank.on_update()
 
     def on_draw(self):
         """ Render the screen. """
@@ -71,24 +136,34 @@ class BlackEarthGame(arcade.Window):
         arcade.start_render()
         # Code to draw the screen goes here
 
-        # Render the player1
-        self.player1.draw()
-        self.player2.draw()
+        # Render the players
+        for player in self.tanksList:
+            player.draw()
 
-        # Render the player1's tank angle
+        # Render the activeTank's tank angle
         arcade.draw_text(
-            text=f"Tank Angle: {self.player1.turretAngleDeg}",
+            text=f"Tank Angle: {self.activeTank.turretAngleDeg}",
             start_x =10.0,
             start_y=0.95*SCREEN_HEIGHT,
             color=arcade.csscolor.WHITE_SMOKE
         )
+        
+        # Display the current player's name
+        arcade.draw_text(
+            text=f"Active: {self.activeTank.name}",
+            start_x=SCREEN_WIDTH/2-50,
+            start_y=0.95*SCREEN_HEIGHT,
+            color=arcade.csscolor.WHITE_SMOKE
+        )
+
+        # Draw other shapes
         self.shapes.draw()
 
 
 def main():
     """ Main method """
     window = BlackEarthGame()
-    window.setup()
+    window.setup(num_tanks=4)
     arcade.run()
 
 
--- game/tank.py
+++ game/tank.py
@@ -1,6 +1,16 @@
-import arcade
-from pymunk import Vec2d
+# General import statements
+import itertools
 
+# Third-party library import statements
+import arcade
+import pymunk
+
+# Make some global variables with general Tank constants
+
+# Note: In general, global variables are very much discouraged, and we
+# should put a TODO: here for us to remove these and put them into, say,
+# a config file or some higher-level function to delegate, but for now
+# they work, and we'll roll with it.
 TANK_SIZE = 50
 TANK_TURRET_LENGTH = 40
 TANK_STARTING_ANGLE_DEG = 45
@@ -8,23 +18,39 @@ TURRET_ANGLE_MAX = 180
 TURRET_ANGLE_MIN = 0
 TURRET_WIDTH = 5
 TURRET_OFFSET_Y = 5
-TURRET_SPEED = 2
+TURRET_SPEED_STEP = 2
+
+# Make an endless iterable of colors to use with Tanks.
+# Note: this may or may not survive; we'll probably think
+# of a better way to do this in the future.
+TANK_COLORS = itertools.cycle([
+    arcade.csscolor.RED,
+    arcade.csscolor.BLUE,
+    arcade.csscolor.GREEN,
+    arcade.csscolor.YELLOW,
+    arcade.csscolor.LAVENDER,
+    arcade.csscolor.DEEP_PINK
+])
 
 class Tank:
     """
     Class encapsulating a player Tank
+
+    Contains data and methods for defining and controlling a
+    tank.
     """
 
-    def __init__(self, position: Vec2d, color: arcade.color):
+    def __init__(self, name: str, position: pymunk.Vec2d, color: arcade.color):
         """
-        Construct the tank with a position and color
+        Construct the tank with a name, position and color
         """
+        self.name = name
         self.size = TANK_SIZE
         self.position = position
         self.color = color
         self.turretAngleDeg = TANK_STARTING_ANGLE_DEG
         self.turretLength = TANK_TURRET_LENGTH
-        self.moveTurret = 0
+        self.turretSpeed = 0
 
     def draw(self):
         """
@@ -42,7 +68,7 @@ class Tank:
         )
         # Draw turret
         # Calculate turret end point
-        turretPosition = Vec2d(self.turretLength, 0)
+        turretPosition = pymunk.Vec2d(self.turretLength, 0)
         turretPosition.rotate_degrees(self.turretAngleDeg)
         arcade.draw_line(
             start_x=self.position.x,
@@ -60,33 +86,36 @@ class Tank:
         If a key is pressed, we'll set a turret movement
         speed. We can't just move the turret, because otherwise the turret will
         only move each and every time that we press a key (meaning, we have to
-        press, release, press, release, just to move twice). Instead, we'll change
+        press, release, press, release, just to move two degrees). Instead, we'll change
         the turret's movement speed based on which keys are pressed.
         """
         if key == arcade.key.LEFT:
-            self.moveTurret += TURRET_SPEED
+            self.turretSpeed = TURRET_SPEED_STEP
         if key == arcade.key.RIGHT:
-            self.moveTurret -= TURRET_SPEED
+            self.turretSpeed = -TURRET_SPEED_STEP
 
     def on_key_release(self, key, modifiers):
         """
         Handle key releases.
 
-        Decrement the turret speed
+        Decrement the turret speed. The equivalent of saying "When!" when
+        your dad is pouring juice.
         """
         if key == arcade.key.LEFT:
-            self.moveTurret -= TURRET_SPEED
+            self.turretSpeed = 0
         if key == arcade.key.RIGHT:
-            self.moveTurret += TURRET_SPEED
+            self.turretSpeed = 0
+        if key == arcade.key.SPACE:
+            self.turretSpeed = 0
 
     def on_update(self):
         """
         Update the player state
 
-        Update the turret angle (and bound it).
+        Update the turret angle (and bound it to a min and max).
         """
-        if self.moveTurret != 0:
-            self.turretAngleDeg += self.moveTurret
+        if self.turretSpeed != 0:
+            self.turretAngleDeg += self.turretSpeed
         if self.turretAngleDeg > TURRET_ANGLE_MAX:
             self.turretAngleDeg = TURRET_ANGLE_MAX
         elif self.turretAngleDeg < TURRET_ANGLE_MIN:
